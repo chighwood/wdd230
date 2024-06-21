b363943671d701639c533d592938d5f71c991f5e
chighwood <hig23003@byui.edu>
2024-06-20 23:14:49 -0500

    All Added things for team assignment

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,12 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
+// menu.js
+
+const hamburgerBtn = document.querySelector("#hamburger");
+const navMenu = document.querySelector(".navMenu");
+
+hamburgerBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    hamburgerBtn.classList.toggle("active");
+});
