ad4683a8ce4f4b713d72c1d7c75146ba3917b7fe
chighwood <hig23003@byui.edu>
2024-07-18 18:42:26 -0500

    Updates to Home Page

---
+++ scripts/links.js
@@ -0,0 +1,40 @@
+const baseURL = "https://chighwood.github.io/wdd230/";
+const linksURL = "https://chighwood.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+  
+  getLinks();
+
+function displayLinks(weeks) {
+  const linksContainer = document.getElementById('links');
+  linksContainer.innerHTML = '';
+
+  weeks.forEach(week => {
+    const weekDiv = document.createElement('div');
+    const weekHeader = document.createElement('h3');
+    weekHeader.textContent = week.week;
+    weekDiv.appendChild(weekHeader);
+
+    const weekLinks = document.createElement('ul');
+    week.links.forEach(link => {
+      const linkItem = document.createElement('li');
+      const linkAnchor = document.createElement('a');
+      linkAnchor.href = baseURL + link.url;
+      linkAnchor.textContent = link.title;
+      linkItem.appendChild(linkAnchor);
+      weekLinks.appendChild(linkItem);
+
+      if (index < week.links.length - 1) {
+        const separator = document.createTextNode(' | ');
+        weekLinks.appendChild(separator);
+      }
+    });
+
+    weekDiv.appendChild(weekLinks);
+    linksContainer.appendChild(weekDiv);
+  });
+}
